255e4c8e5fb7ca77f7078e111a406f27e7023961
sanr <oleksandr.lishchynskyy@ria.com>
2023-08-19 16:47:38 +0300

    Tasks complete

---
+++ lesson2_task07.erl
@@ -0,0 +1,24 @@
+-module(lesson2_task07).
+-export([flatten/1]).
+-import(lesson2_task05, [reverse/1]).
+
+flatten(List)->
+    reverse(flatten(List, [])).
+
+flatten([[]|Tail], Acc)->
+    flatten(Tail, Acc);
+flatten([[H|T]|Tail], Acc)->
+    Acc2 = flatten(T,[H|Acc]),
+    flatten(Tail, Acc2);
+flatten([H|Tail], Acc)->
+    flatten(Tail, [H|Acc]);
+flatten([], Acc)->
+    Acc.
+
+
+% reverse(List)->
+%     reverse(List,[]).
+% reverse([H|T], Acc)->
+%     reverse(T, [H|Acc]);
+% reverse([], Acc)->
+%     Acc.
